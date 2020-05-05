eadce3078852fa7bd19aad89d4be079e93f6b3b6
leandro.godoy <leandro.godoy@acct.global>
2020-05-04 21:45:30 -0300

    fix register form

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -45,9 +45,9 @@ export default function Register() {
                     <h1>Cadastro</h1>
                     <p>Faça seu cadastro, entre na plataforma e ajude pessoas a encontrarem os casos da sua ONG.</p>
 
-                    <Link className="back-link" to="/m start">
+                    <Link className="back-link" to="/">
                         <FiArrowLeft size={16} color='#E02041' />
-                        Não tenho cadastro
+                        Já tenho cadastro
                     </Link>
                 </section>
                 <form onSubmit={HandleRegister}>
@@ -63,14 +63,14 @@ export default function Register() {
                         onChange={e => setEmail(e.target.value)}
                     />
                     <input
-                        placeholder="whatsapp"
+                        placeholder="Telefone"
                         value={whatsapp}
                         onChange={e => setWhatsapp(e.target.value)}
                     />
 
                     <div className="input-group">
                         <input
-                            placeholder="Nome da Cidade"
+                            placeholder="Cidade"
                             value={city}
                             onChange={e => setCity(e.target.value)}
                         />
