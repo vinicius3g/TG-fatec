41646e9856719c5981695935f19a0d0d0bf7cbd7
vinicius dos santos paixao <vinicius.ds.paixao@gmail.com>
2020-05-17 16:22:47 -0300

    remoção de backend

--- backend/tests/unit/gererateUniqueId.spec.js
+++
@@ -1,9 +0,0 @@
-const generateUniqueId = require('../../src/utils/gerarateUniqueId');
-
-describe('Generate Unique ID', () => {
-    it('should generate an unique ID', () => {
-        const id = generateUniqueId();
-
-        expect(id).toHaveLength(8)
-    })
-});
